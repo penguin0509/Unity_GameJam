fa4e7bdde143326781265db21bf3e3f162af42d9
DESKTOP-RGS91TI\user <sixt2004@gmail.com>
2021-06-13 00:11:41 +0800

    Shoot 80%

---
+++ Progam/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Bullet : MonoBehaviour
+{
+
+    ShootRope gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun = GameObject.FindGameObjectWithTag("Gun").GetComponent<ShootRope>();
+    }
+
+    // Update is called once per frame
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "weapon")
+        {
+            gun.TargetHit(other.gameObject);
+            ShootRope.weapon1TF = true;
+            ShootRope.ShootTF = false;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "weapon1")
+        {
+            gun.TargetHit(other.gameObject);
+            ShootRope.ShootTF = false;
+            ShootRope.weapon2TF = true;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Ground")
+        {
+            gun.TargetHit(other.gameObject);
+            ShootRope.ShootTF = false;
+            ShootRope.Cube1TF = true;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Ground2")
+        {
+            gun.TargetHit(other.gameObject);
+            ShootRope.ShootTF = false;
+            ShootRope.Cube2TF = true;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Ground3")
+        {
+            gun.TargetHit(other.gameObject);
+            ShootRope.ShootTF = false;
+            ShootRope.Cube3TF = true;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "leftRight")
+        {
+            ShootRope.ShootTF = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Progam/ShootRope.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShootRope : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Cube1;
+    public GameObject Cube2;
+    public GameObject Cube3;
+    public GameObject weapon1;
+    public GameObject weapon2;
+    public GameObject Bullet;
+
+    public static bool Cube1TF;
+    public static bool Cube2TF;
+    public static bool Cube3TF;
+    public static bool weapon1TF;
+    public static bool weapon2TF;
+
+
+    public float BulletSpeed;
+
+    public Transform ShootPoint;
+
+    Vector2 Direction;
+
+    public LineRenderer line;
+    GameObject target;
+
+    public static bool ShootTF = true;
+
+    public SpringJoint2D spring;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        line.enabled = false;
+        spring.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Direction = MousePos - (Vector2)transform.position;
+
+        FaceMouse();
+
+        if (Input.GetMouseButtonDown(0) && ShootTF == true)
+        {
+            ShootTF = false;
+            Shoot();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Release();
+            Cube2TF = false;
+            Cube1TF = false;
+            Cube3TF = false;
+            weapon1TF = false;
+            weapon2TF = false;
+            mobile.mobileTF = false;
+            mobile.jumpTF = false;
+            ShootTF = true;
+        }
+
+        if (target != null)
+        {
+            if (Cube1TF == true)
+            {
+                mobile.jumpTF = true;
+                mobile.mobileTF = true;
+                line.SetPosition(0, ShootPoint.position);
+                line.SetPosition(1, target.transform.position);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                worldPosition.x += 8;
+                Cube1.transform.localPosition = new Vector3(worldPosition.x, worldPosition.y, 11.713f);
+            }
+            else if (Cube2TF == true)
+            {
+                mobile.jumpTF = true;
+                mobile.mobileTF = true;
+                line.SetPosition(0, ShootPoint.position);
+                line.SetPosition(1, target.transform.position);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                worldPosition.x += 8;
+                Cube2.transform.localPosition = new Vector3(worldPosition.x, worldPosition.y, 11.713f);
+            }
+            else if (Cube3TF == true)
+            {
+                mobile.jumpTF = true;
+                mobile.mobileTF = true;
+                line.SetPosition(0, ShootPoint.position);
+                line.SetPosition(1, target.transform.position);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                worldPosition.x += 8;
+                Cube3.transform.localPosition = new Vector3(worldPosition.x, worldPosition.y, 11.713f);
+            }
+            else if (weapon1TF == true)
+            {
+                mobile.jumpTF = true;
+                mobile.mobileTF = true;
+                line.SetPosition(0, ShootPoint.position);
+                line.SetPosition(1, target.transform.position);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                worldPosition.x += 8;
+                weapon1.transform.localPosition = new Vector3(worldPosition.x, worldPosition.y, 11.713f);
+
+
+            }
+            else if (weapon2TF == true)
+            {
+                mobile.jumpTF = true;
+                mobile.mobileTF = true;
+                line.SetPosition(0, ShootPoint.position);
+                line.SetPosition(1, target.transform.position);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                worldPosition.x += 8;
+                weapon2.transform.localPosition = new Vector3(worldPosition.x, worldPosition.y, 11.713f);
+            }
+        }
+    }
+
+    void FaceMouse()
+    {
+        transform.right = Direction;
+    }
+
+    void Shoot()
+    {
+        
+        GameObject BulletIns =  Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(transform.right * BulletSpeed);
+
+
+    }
+
+    public void TargetHit(GameObject hit)
+    {
+        target = hit;
+        line.enabled = true;
+        spring.enabled = true;
+        spring.connectedBody = target.GetComponent<Rigidbody2D>();
+    }
+
+    void Release()
+    {
+        line.enabled = false;
+        spring.enabled = false;
+        target = null;
+    }
+
+}
