ba583c6a84b875aa115c8987e6a8b9b3f67804d1
DESKTOP-RGS91TI\user <sixt2004@gmail.com>
2021-06-13 17:33:37 +0800

    Game Menu[Finish]

---
+++ Menu/Script/StartController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    
+    void Start()
+    {
+       
+
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                ClickEvent();
+            });
+        
+    }
+
+    // Update is called once per frame
+    void ClickEvent()
+	{
+        
+        SceneManager.LoadScene(1);
+	}
+
+	void Update()
+	{
+		
+	}
+}
